a741625d1e7e38c60a45b6f8c0bf1c113f0155af
Yana <61992820+roddziuk@users.noreply.github.com>
2020-05-14 14:23:32 +0200

    Create Wilder.java

---
+++ Wilder.java
@@ -0,0 +1,31 @@
+  public class Wilder {
+    
+  private String firstName;
+  private boolean aware;
+
+  public Wilder (String firstName, boolean aware) {
+    
+    this.firstName = firstName;
+    this.aware = aware;  
+  }
+
+  public void setFirstName (String firstName) {
+    this.firstName = firstName;
+  }
+  public void setAware (boolean aware) {
+    this.aware = aware;
+  }
+  public String getFirstName () {
+    return this.firstName;
+  }
+  public boolean isAware () {
+    return this.aware;
+  }
+  public String whoAmI () {
+    if (this.isAware() == true) {
+      return "Je m'appelle " + this.getFirstName() + " et je suis aware ";
+    } else {
+      return "Je m'appelle " + this.getFirstName() + " et je ne suis pas aware";
+      }
+    }
+  }
